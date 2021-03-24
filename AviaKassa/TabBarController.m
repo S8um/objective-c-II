bfdcb9a66d4acb2c0c834169512edea2c434fc55
GRoman <grromman@gmail.com>
2021-03-24 09:19:17 +0500

    Step 2

--- AviaKassa/TabBarController.m
+++ AviaKassa/TabBarController.m
@@ -6,6 +6,8 @@
 //
 
 #import "TabBarController.h"
+#import "SecondViewController.h"
+#import "TableViewController.h"
 
 @interface TabBarController ()
 
@@ -13,19 +15,41 @@
 
 @implementation TabBarController
 
+- (instancetype)init {
+    self = [super initWithNibName:nil bundle:nil];
+    if (self) {
+        self.viewControllers = [self createViewControllers];
+        self.tabBar.tintColor= [UIColor blueColor];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Private
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSArray<UIViewController *> *)createViewControllers {
+    NSMutableArray<UIViewController *> *controllers = [NSMutableArray new];
+    
+    SecondViewController *secondViewController = [SecondViewController new];
+    secondViewController.viewTitle = @"Days of week";
+    secondViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"First" image:[UIImage systemImageNamed:(@"folder")] selectedImage:[UIImage systemImageNamed:@"folder.fil"]];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:secondViewController];
+    
+    [controllers addObject:navigationController];
+    
+    
+    TableViewController *tableViewController = [TableViewController new];
+    tableViewController.viewTitle = @"Month";
+    tableViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Second" image:[UIImage systemImageNamed:(@"folder")] selectedImage:[UIImage systemImageNamed:@"folder.fil"]];
+    UINavigationController *tableNavigationTController = [[UINavigationController alloc] initWithRootViewController:tableViewController];
+    
+    [controllers addObject:tableNavigationTController];
+    
+    return controllers;
 }
-*/
 
 @end
